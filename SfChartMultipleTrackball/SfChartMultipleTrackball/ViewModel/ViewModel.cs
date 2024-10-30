437e9f0a988b4adff12c62881effb7f4fe746b87
subash_s <subash.s@syncfusion.com>
2024-10-30 11:24:19 +0530

    added ui customization

--- SfChartMultipleTrackball/SfChartMultipleTrackball/ViewModel/ViewModel.cs
+++ SfChartMultipleTrackball/SfChartMultipleTrackball/ViewModel/ViewModel.cs
@@ -10,8 +10,8 @@
             {
                 new Model { Day = "Monday", CPULoad = 35 },
                 new Model { Day = "Tuesday", CPULoad = 42 },
-                new Model { Day = "Wednesday", CPULoad = 18 },
-                new Model { Day = "Thursday", CPULoad = 30 },
+                new Model { Day = "Wednesday", CPULoad = 28 },
+                new Model { Day = "Thursday", CPULoad = 40 },
                 new Model { Day = "Friday", CPULoad = 64 },
                 new Model { Day = "Saturday", CPULoad = 22 },
                 new Model { Day = "Sunday", CPULoad = 10 }
